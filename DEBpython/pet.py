cd514bf099e077c2e0a02b510ad56b2b1fa896d8
Diogo Oliveira <diogo.oliveira898@gmail.com>
2025-06-09 19:01:15 +0300

    Update Pet class to work with Environment and State classes
    - Temperature rates are now dependent on the temperature defined in State
    - Physical properties are now computed based on state variable values in State

--- DEBpython/pet.py
+++ DEBpython/pet.py
@@ -1,4 +1,5 @@
-from .composition import Composition, Compound, RuminantComposition
+from .composition import Composition, RuminantComposition
+from .state import State
 
 import numpy as np
 from math import exp
@@ -22,9 +23,12 @@ class Pet:
         Nº Molecules -> mol (moles)
     """
     temperature_affected = ('p_Am', 'v', 'p_M', 'p_T', 'k_J')
+    T_REF = 293.15  # Reference temperature (K)
 
-    def __init__(self, p_Am, kap, v, p_M, E_G, k_J, E_Hb, E_Hp, kap_R, comp=None, p_T=0, kap_X=0.8, kap_P=0.1,
-                 E_0=1e6, V_0=1e-12, T_A=8000, T_ref=293.15, T=298.15, del_M=1, **additional_parameters):
+    def __init__(self, p_Am, kap, v, p_M, E_G, k_J, E_Hb, E_Hp, kap_R, state=None, comp=None, p_T=0, kap_X=0.8,
+                 kap_P=0.1, T_typical=T_REF, E_0=1e6, V_0=1e-12, T_A=8000, del_M=1, **additional_parameters):
+
+        # Parameters
         self.E_G = E_G  # Specific cost for Structure (J/cm^3)
         self._p_Am = p_Am  # Surface-specific maximum assimilation rate (J/d.cm^2)
         self._v = v  # Energy conductance (cm/d)
@@ -40,13 +44,18 @@ class Pet:
         self.kap_X = kap_X  # Digestion efficiency (-)
         self.kap_P = kap_P  # Defecation efficiency (-)
         self.T_A = T_A  # Arrhenius temperature (K)
-        self.T_ref = T_ref  # Reference temperature (K)
-        self.T = T  # Temperature (K)
+        self.T_typical = T_typical  # Typical body temperature (K)
         self.del_M = del_M  # Shape coefficient (-)
+
+        # Model inputs are saved in State
+        if state is None:
+            state = State()
+            state.T = T_typical
+        self.state = state
 
         # Chemical composition
         if comp is None:
-            self.comp = Composition()
+            self.comp = Composition(food=self.state.food_comp)
         elif isinstance(comp, (list, tuple)):
             self.comp = Composition(*comp)
         elif isinstance(comp, dict):
@@ -103,6 +112,10 @@ class Pet:
         """
         return V ** (1 / 3) / self.del_M
 
+    @property
+    def physical_length(self):
+        return self.state.V ** (1 / 3) / self.del_M
+
     def compute_physical_volume(self, V, E, E_R):
         """
         Converts structure, reserve and reproduction buffer into physical volume.
@@ -113,6 +126,11 @@ class Pet:
         """
         return V + (E + E_R) * self.comp.E.w / self.comp.E.d / self.comp.E.mu
 
+    @property
+    def physical_volume(self):
+        E, V, E_R = self.state.E, self.state.V, self.state.E_R
+        return V + (E + E_R) * self.comp.E.w / self.comp.E.d / self.comp.E.mu
+
     def compute_wet_weight(self, V, E, E_R):
         """
         Converts structure, reserve and reproduction buffer into wet weight_col. Assumes density of reserve d_V equal to 1
@@ -121,17 +139,30 @@ class Pet:
         :param E_R: scalar or vector of reproduction buffer
         :return: scalar or vector of wet weight_col
         """
+        # TODO: Compute wet weight with reproduction buffer (check DEB comments)
+        # TODO: Check assumption of d_V = 1, seems like it is wet density of structure
+        scaled_reserved_density = (E / V) / self.E_m
+        return V * (1 + scaled_reserved_density * self.omega)
+
+    @property
+    def wet_weight(self):
+        E, V, E_R = self.state.E, self.state.V, self.state.E_R
         scaled_reserved_density = (E / V) / self.E_m
         return V * (1 + scaled_reserved_density * self.omega)
 
     def compute_dry_weight(self, V, E, E_R):
         """
-        Converts structure, reserve and reproduction buffer into dry weight_col.
+        Converts structure, reserve and reproduction buffer into dry_weight.
         :param V: scalar or vector of structure
         :param E: scalar or vector of reserve
         :param E_R: scalar or vector of reproduction buffer
         :return: scalar or vector of wet weight_col
         """
+        return 1 * V + (E + E_R) * self.comp.E.w / self.comp.E.mu
+
+    @property
+    def dry_weight(self):
+        E, V, E_R = self.state.E, self.state.V, self.state.E_R
         return 1 * V + (E + E_R) * self.comp.E.w / self.comp.E.mu
 
     def __getattr__(self, item):
@@ -148,7 +179,7 @@ class Pet:
         """Called when print(Pet) is called. Returns a description of the Pet, including the most relevant parameters
         and chemical reactions."""
         # TODO: Add symbols in front of description or replace
-        description = f"Parameters at T={self.T} K\n" \
+        description = f"Parameters at T={self.state.T} K\n" \
                       f"Surface-specific maximum assimilation rate: {self.p_Am:.6} (J/d.cm^2)\n" \
                       f"Allocation fraction to soma: {self.kap} (-)\n" \
                       f"Energy conductance: {self.v:.6} (cm/d)\n" \
@@ -200,6 +231,8 @@ class Pet:
         for reaction_name, formula in reactions.items():
             print(f"{reaction_name.capitalize()}: {formula}")
 
+    # TODO: scaled functional response based on p_X from state
+
     @property
     def E_m(self):
         """Maximum energy density (J/cm^3)."""
@@ -237,15 +270,20 @@ class Pet:
 
     @property
     def p_Xm(self):
-        """Specific maximum ingestion rate (J/d.cm^2)"""
+        """Specific maximum ingestion rate (J/d.cm^2)."""
         return self.p_Am / self.kap_X
+
+    @property
+    def f(self):
+        """Scaled functional response (-)."""
+        return self.state.p_X / (self.state.V ** (2 / 3) * self.p_Xm)
 
     @property
     def TC(self):
         """Temperature correction factor TC (-)."""
-        return exp(self.T_A / self.T_ref - self.T_A / self.T)
+        return exp(self.T_A / self.T_REF - self.T_A / self.state.T)
 
-    def r_B(self, f=1):
+    def r_B(self, f):
         """Von Bertalanffy growth rate r_B (d^-1)."""
         return self.k_M / (1 + f / self.g) / 3
 
@@ -304,7 +342,7 @@ class Pet:
         """Heating Length (cm)."""
         return self._p_T / self._p_M
 
-    def L_inf(self, f=1):
+    def L_inf(self, f):
         """Ultimate Length (cm)."""
         return f * self.L_m - self.L_T
 
@@ -322,20 +360,23 @@ class Pet:
         return 1 - self.kap_X - self.kap_P - self.kap_N
 
 
+# TODO: Update Pet class to use new Environment and State paradigm
 class Ruminant(Pet):
     """
         class Ruminant:
 
         Child class of Pet.
         Assumes assimilation occurs in two sub transformations, one that produces CO2 and another that produces CH4. The
-        assimilation reaction is a weighted average of both sub transformations.
+        assimilation reaction is an average of both sub transformations, weighted by the parameter xi_C.
         """
 
     def __init__(self, p_Am, kap, v, p_M, E_G, k_J, E_Hb, E_Hp, kap_R, xi_C, comp=None, p_T=0, kap_X=0.8,
-                 kap_P=0.1, E_0=1e6, V_0=1e-12, T_A=8000, T_ref=293.15, T=298.15, del_M=1, **additional_parameters):
+                 kap_P=0.1, E_0=1e6, V_0=1e-12, T_A=8000, T_ref=293.15, T_typical=298.15, del_M=1,
+                 **additional_parameters):
 
         super().__init__(p_Am, kap, v, p_M, E_G, k_J, E_Hb, E_Hp, kap_R, comp=comp, p_T=p_T, kap_X=kap_X, kap_P=kap_P,
-                         E_0=E_0, V_0=V_0, T_A=T_A, T_ref=T_ref, T=T, del_M=del_M, **additional_parameters)
+                         E_0=E_0, V_0=V_0, T_A=T_A, T_ref=T_ref, T_typical=T_typical, del_M=del_M,
+                         **additional_parameters)
         # Chemical composition
         if comp is None:
             self.comp = RuminantComposition()
@@ -402,33 +443,36 @@ class Ruminant(Pet):
     def kap_Q(self):
         return 1 - self.kap_X - self.kap_P - self.kap_N - self.kap_M
 
-# Dictionary with parameters for several commom organisms. Usage with Pet class is: Pet(**animals[pet_name])
+
+# Dictionary with parameters for several common organisms. Usage with Pet class is: Pet(**animals[pet_name])
 animals = {
     'shark': dict(E_G=5212.32, p_Am=558.824, v=0.02774, p_M=34.3632, kap=0.84851, k_J=0.002, kap_R=0.95, E_Hb=7096,
-                  E_Hp=300600, E_0=174_619, T=282.15),
+                  E_Hp=300600, E_0=174_619, T_typical=282.15),
     'muskox': dict(E_G=7842.44, p_Am=1053.62, v=0.13958, p_M=18.4042, kap=0.82731, k_J=0.00087827, kap_R=0.95,
-                   E_Hb=1.409e+7, E_Hp=3.675e+8, E_Hx=5.136e+7, t_0=18.2498, f_milk=1, T=310.85),
+                   E_Hb=1.409e+7, E_Hp=3.675e+8, E_Hx=5.136e+7, t_0=18.2498, f_milk=1, T_typical=310.85),
     'human': dict(E_G=7879.55, p_Am=118.992, v=0.031461, p_M=2.5826, kap=0.78656, k_J=0.00026254, kap_R=0.95,
                   E_Hb=4.81e+6, E_Hp=8.726e+7, E_Hx=1.346e+7, t_0=26.8217, f_milk=1),
     'bos_taurus_alentejana': dict(E_G=8261.79, p_Am=2501.03, v=0.107224, p_M=42.2556, kap=0.976264, k_J=0.002,
                                   kap_R=0.95, E_Hb=2071229.972, E_Hp=30724119.81, E_Hx=15139260.45, t_0=109.4715964,
-                                  f_milk=1, del_M=0.349222, kap_X=0.3, rum_fraction=0.3, T=311.75),
+                                  f_milk=1, del_M=0.349222, kap_X=0.3, rum_fraction=0.3, T_typical=311.75),
     'bos_taurus_angus': dict(E_G=7960, p_Am=1584.86, v=0.08859, p_M=21.81, kap=0.9633,
                              k_J=0.002, kap_R=0.95, E_Hb=4.335e+06, E_Hp=2.608e+07, E_Hx=2.083e+07,
-                             t_0=84.66, f_milk=1, del_M=0.4481, kap_X=0.09051, rum_fraction=0.3, T=311.75),
+                             t_0=84.66, f_milk=1, del_M=0.4481, kap_X=0.09051, rum_fraction=0.3, T_typical=311.75),
     'bos_taurus_limousin': dict(E_G=8839.515768, p_Am=4927.102094, v=0.167427252, p_M=88.18065702, kap=0.9783141238,
                                 k_J=0.002, kap_R=0.95, E_Hb=2234700.015, E_Hp=91160096.48, E_Hx=34941987.33,
-                                t_0=188.2442861, f_milk=1, del_M=0.3686000612, kap_X=0.3, rum_fraction=0.3, T=311.75),
+                                t_0=188.2442861, f_milk=1, del_M=0.3686000612, kap_X=0.3, rum_fraction=0.3,
+                                T_typical=311.75),
     'bos_taurus_charolais': dict(E_G=8885.233383, p_Am=4583.16235, v=0.06126486348, p_M=96.28243777, kap=0.9771477494,
                                  k_J=0.002, kap_R=0.95, E_Hb=2031342.633, E_Hp=26960202.1, E_Hx=17219252.94,
-                                 t_0=88.84268928, f_milk=1, del_M=0.2688914292, kap_X=0.3, rum_fraction=0.3, T=311.75),
+                                 t_0=88.84268928, f_milk=1, del_M=0.2688914292, kap_X=0.3, rum_fraction=0.3,
+                                 T_typical=311.75),
     'bos_taurus_mertolenga': dict(E_G=8880, p_Am=1787, v=0.2361, p_M=24.16, kap=0.9528, k_J=0.002, kap_R=0.95,
                                   E_Hb=5.948e+06, E_Hp=1.467e+08, E_Hx=4.451e+07, t_0=229, f_milk=1, del_M=0.4,
-                                  kap_X=0.1815, rum_fraction=0.3, T=311.75),
+                                  kap_X=0.1815, rum_fraction=0.3, T_typical=311.75),
     'sheep': dict(E_G=7838.53, p_Am=3473.31, v=0.18751, p_M=96.0278, kap=0.82933, k_J=0.002, kap_R=0.95,
                   E_Hb=4.154e+06, E_Hp=2.397e+08, E_Hx=6.797e+07, t_0=80.9564, f_milk=1, del_M=0.2688914292, kap_X=0.8,
-                  rum_fraction=0.3, T=311.75),
+                  rum_fraction=0.3, T_typical=311.75),
     'ovis_aries_lacaune': dict(E_G=7828, p_Am=2289.55, v=1.845, p_M=57.54, kap=0.955, k_J=0.002, kap_R=0.95,
                                E_Hb=1.656e+06, E_Hp=6.298e+07, E_Hx=6.345e+06, t_0=126.5, f_milk=1, del_M=0.2688914292,
-                               kap_X=0.8, rum_fraction=0.3, T=311.75),
+                               kap_X=0.8, rum_fraction=0.3, T_typical=311.75),
 }
